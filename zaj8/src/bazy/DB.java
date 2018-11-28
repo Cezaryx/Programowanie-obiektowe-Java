f51acf239c95f80c840ad18c3ca0d50906f73ef3
Cezaryx <cezaryxmaster@gmail.com>
2018-11-28 12:23:33 +0100

    zadanie wykonane

--- zaj8/src/bazy/DB.java
+++ zaj8/src/bazy/DB.java
@@ -7,18 +7,152 @@ public class DB {
     private Statement stmt = null;
     private ResultSet rs = null;
 
-    public void connect(){
-        try{
-            Class.forName("com.mysql.jdbc.Driver").newInstance();
-            conn = DriverManager.getConnection("jdbc:mysql://mysql.agh.edu.pl/nazwa_bazy", "username", "password ");
-        }catch(SQLException ex){
-            System.out.println("SQLException: " +ex.getMessage());
+    public void connect() {
+        try {
+            //Class.forName("com.mysql.jdbc.Driver").newInstance();
+            conn = DriverManager.getConnection("jdbc:mysql://mysql.agh.edu.pl/cezarysn", "cezarysn", "DxsiCcmfqFdTnkGn");
+        } catch (SQLException ex) {
+            System.out.println("SQLException: " + ex.getMessage());
             System.out.println("SQLState: " + ex.getSQLState());
             System.out.println("VendorError: " + ex.getErrorCode());
-        }catch(Exception e){e.printStackTrace();}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-    public String printAll(){
+
+    public String printAll() {
         String str = new String();
-        return str;
+        try {
+            connect();
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery("SELECT * FROM books");
+            while (rs.next()) {
+                str =str + rs.getString(1);
+                str =str+" | " + rs.getString(2);
+                str =str+" | " + rs.getString(3);
+                str =str+" | " + rs.getString(4)+"\n";
+            }
+        } catch (SQLException ex) {
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        } finally {
+            // zwalniamy zasoby, które nie będą potrzebne
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException sqlEx) {
+                } // ignore
+                rs = null;
+            }
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException sqlEx) {
+                } // ignore
+                stmt = null;
+            }
+
+            return str;
+        }
+    }
+    public String printISBN(String wzorzec) {
+        String str = new String();
+        try {
+            connect();
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery("SELECT * FROM books where ISBN='"+wzorzec+"'");
+            while (rs.next()) {
+                str =str + rs.getString(1);
+                str =str+" | " + rs.getString(2);
+                str =str+" | " + rs.getString(3);
+                str =str+" | " + rs.getString(4)+"\n";
+            }
+        } catch (SQLException ex) {
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        } finally {
+            // zwalniamy zasoby, które nie będą potrzebne
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException sqlEx) {
+                } // ignore
+                rs = null;
+            }
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException sqlEx) {
+                } // ignore
+                stmt = null;
+            }
+
+            return str;
+        }
+    }
+    public String printnazwisko(String wzorzec) {
+        String str = new String();
+        try {
+            connect();
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery("SELECT * FROM books where author LIKE '%_"+wzorzec+"'");
+            while (rs.next()) {
+                str =str + rs.getString(1);
+                str =str+" | " + rs.getString(2);
+                str =str+" | " + rs.getString(3);
+                str =str+" | " + rs.getString(4)+"\n";
+            }
+        } catch (SQLException ex) {
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        } finally {
+            // zwalniamy zasoby, które nie będą potrzebne
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException sqlEx) {
+                } // ignore
+                rs = null;
+            }
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException sqlEx) {
+                } // ignore
+                stmt = null;
+            }
+
+            return str;
+        }
+    }
+    public void add_da_book(String a,String b,String c,String d){
+        try {
+            connect();
+            stmt = conn.createStatement();
+            stmt.executeUpdate("INSERT INTO `books`(`isbn`, `title`, `author`, `year`) VALUES ("+a+",'"+b+"','"+c+"',"+d+")");
+        } catch (SQLException ex) {
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        } finally {
+            // zwalniamy zasoby, które nie będą potrzebne
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException sqlEx) {
+                } // ignore
+                rs = null;
+            }
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException sqlEx) {
+                } // ignore
+                stmt = null;
+            }
+        }
     }
 }
--- zaj8/src/bazy/Main.java
+++ zaj8/src/bazy/Main.java
@@ -2,38 +2,98 @@ package bazy;
 
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+
+
 public class Main extends Application {
+    DB db = new DB();
+    GridPane gridPane = new GridPane();
+    TextField txf1 = new TextField("ISBN");
+    TextField txf2 = new TextField("Nazwisko");
+    TextField txf3 = new TextField("ISBN");
+    TextField txf4 = new TextField("Tytuł");
+    TextField txf5 = new TextField("Autor");
+    TextField txf6 = new TextField("Rok");
     @Override
     public void start(Stage stage){
+        EventHandler<MouseEvent> eventHandler1 = new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent e) {
+                gridPane.getChildren().remove(14);
+                String wypisz = new String();
+                wypisz =db.printAll();
+                Text ttext = new Text(wypisz);
+                gridPane.add(ttext,0,4);
+            }
+        };
+
+        EventHandler<MouseEvent> eventHandler2 = new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent e) {
+                gridPane.getChildren().remove(14);
+                String wzorzec = txf1.getText();
+                String wypisz = new String();
+                wypisz =db.printISBN(wzorzec);
+                Text ttext = new Text(wypisz);
+                gridPane.add(ttext,0,4);
+            }
+        };
+
+        EventHandler<MouseEvent> eventHandler3 = new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent e) {
+                gridPane.getChildren().remove(14);
+                String wzorzec = txf2.getText();
+                String wypisz = new String();
+                wypisz =db.printnazwisko(wzorzec);
+                Text ttext = new Text(wypisz);
+                gridPane.add(ttext,0,4);
+            }
+        };
+
+        EventHandler<MouseEvent> eventHandler4 = new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent e) {
+                gridPane.getChildren().remove(14);
+                String a = txf3.getText();
+                String b = txf4.getText();
+                String c = txf5.getText();
+                String d = txf6.getText();
+                db.add_da_book(a,b,c,d);
+                Text ttext = new Text("");
+                gridPane.add(ttext,0,4);
+            }
+        };
+
         Text tx1 = new Text("Wypisz całą bazę danych");
         Button bt1 = new Button("Wypisz");
+        bt1.addEventFilter(MouseEvent.MOUSE_CLICKED, eventHandler1);
         /*pobierz mySQL builder do działania połączenia*/
         /*pooglądaj: Java tutorial 20: connect to mysql */
         /* ctrl + shift + alt +s >>> libraries >>> dodaj mysql connector */
         Text tx2 = new Text("Wyszukaj po numerze ISBN");
-        TextField txf1 = new TextField("ISBN");
+
         Button bt2 = new Button("Wyszukaj");
-
+        bt2.addEventFilter(MouseEvent.MOUSE_CLICKED, eventHandler2);
         Text tx3 = new Text("Wyszukaj po nazwisku autora");
-        TextField txf2 = new TextField("Nazwisko");
+
         Button bt3 = new Button("Wyszukaj");
-
+        bt3.addEventFilter(MouseEvent.MOUSE_CLICKED, eventHandler3);
         Text tx4 = new Text("Dodaj rekord");
-        TextField txf3 = new TextField("ISBN");
-        TextField txf4 = new TextField("Tytuł");
-        TextField txf5 = new TextField("Autor");
-        TextField txf6 = new TextField("Rok");
-        Button bt4 = new Button("Dodaj");
 
-        GridPane gridPane = new GridPane();
+        Button bt4 = new Button("Dodaj");
+        Text ttext = new Text("");
+        bt4.addEventFilter(MouseEvent.MOUSE_CLICKED,eventHandler4);
+
         gridPane.add(tx1,0,0);
         gridPane.add(bt1,1,0);
 
@@ -51,11 +111,11 @@ public class Main extends Application {
         gridPane.add(txf5,3,3);
         gridPane.add(txf6,4,3);
         gridPane.add(bt4,5,3);
-
+        gridPane.add(ttext,0,4);
 
         Group root = new Group();
         root.getChildren().addAll(gridPane);
-        Scene scene = new Scene(root, 1000, 1000);
+        Scene scene = new Scene(root, 1500, 800);
         stage.setTitle("Baza danych");
         stage.setScene(scene);
         stage.show();
